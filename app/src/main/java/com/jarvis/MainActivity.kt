5c50ba9369e594a08fc7a358befa7bb75be12c12
edisonai429-bit <edisonai429@gmail.com>
2025-11-25 14:40:26 +0000

    Add minimal MainActivity and AndroidManifest

---
+++ app/src/main/java/com/jarvis/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.jarvis import 
+android.os.Bundle import 
+androidx.appcompat.app.AppCompatActivity 
+class MainActivity : AppCompatActivity() 
+{
+    override fun 
+    onCreate(savedInstanceState: 
+    Bundle?) {
+        super.onCreate(savedInstanceState) 
+        setContentView(android.R.layout.simple_list_item_1) 
+        // simple default layout
+    }
+}
