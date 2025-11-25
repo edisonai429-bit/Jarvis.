36c8b64282fd69a0b17cf16cad19d4c5ff11d969
edisonai429-bit <edisonai429@gmail.com>
2025-11-25 15:47:21 +0000

    Add OkHttp dependency for OpenAI API

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,3 +10,8 @@ allprojects { repositories { google()
         mavenCentral()
     }
 }
+dependencies {
+        implementation("org.jetbrains.kotlin:kotlin-stdlib:1.9.0")
+            implementation("com.squareup.okhttp3:okhttp:4.11.0") // add this line
+}
+}
